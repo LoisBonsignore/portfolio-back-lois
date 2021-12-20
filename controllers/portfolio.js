34cbf9ffa3fd10ead69d174efab37ef1eee7b367
Lois Bonsignore <bonsignore.lois@gmail.com>
2021-12-20 14:58:22 +0100

    modif

--- controllers/portfolio.js
+++
@@ -1,8 +0,0 @@
-const Projects = require("../models/portfolio")
-
-
-exports.getProjects = (req, res, next) => {
-    Projects.find({}).then((projects) => {
-        res.send(projects);
-    });
-};
--- routes/portfolio.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const portfolio = require("../controllers/portfolio");
-
-router.get('/projects', portfolio.getProjects)
-
-module.exports = router;
